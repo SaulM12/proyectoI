e8c9aaaf4d2b2046b6b30c4a986af4da8c2d67ff
Usuario <Usuario@DESKTOP-1V9AS6J>
2020-07-01 11:35:22 -0500

    good

--- src/test/java/com/unibe/edu/proyect/resources/StudentResourceTest.java
+++ src/test/java/com/unibe/edu/proyect/resources/StudentResourceTest.java
@@ -1,9 +1,10 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.unibe.edu.proyect.resources;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,31 +12,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.unibe.edu.proyect.dtos.StudentDto;
-import com.unibe.edu.proyect.resources.RestService;
-import com.unibe.edu.proyect.resources.StudentResource;
-
-
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentResourceTest {
 
 	@Rule
-	public ExpectedException thrown= ExpectedException.none();
-	
+	public ExpectedException thrown = ExpectedException.none();
+
 	@Autowired
 	private RestService restService;
 	private StudentDto studentDto;
-	
+
 	@Before
 	public void before() {
-		this.studentDto= new StudentDto(1, "1727171404", "Jose Jose", "Jaramillo Perez", "kasj@gmail", "0969043552", "2020-04-03", "Caderon");
+		this.studentDto = new StudentDto(1, "545", "SAMFDKS JNDJS", "SAKSM JSND", "SDLS", "545456", "2000-04-03",
+				"SLKSDLKS");
 	}
-	
+
 	@Test
-	public void createStudentTest() {
+	public void createProductTest() {
 		restService.restBuilder().path(StudentResource.STUDENT).body(studentDto).post().build();
 	}
-	
 }
